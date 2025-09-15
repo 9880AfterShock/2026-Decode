1cf8c8056b10615399af0f669f7cc18c8121f61d
BiggerBozo <cflibftc@gmail.com>
2025-09-15 12:44:28 -0700

    Working on setting up alignment class (https://rr.brott.dev/docs/v1-0/guides/teleop-actions/)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Aiming/Alignment.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.Aiming;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.InstantAction;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Alignment {
+    private static OpMode opmode;
+    private static FtcDashboard dash = FtcDashboard.getInstance();
+    private static List<Action> runningActions = new ArrayList<>();
+
+    public static void initAlignment(OpMode opmode) { // init motors
+        Alignment.opmode = opmode;
+    }
+
+    public static void updateAlignment(){
+        TelemetryPacket packet = new TelemetryPacket();
+
+        // updated based on gamepads
+
+        // update running actions
+        List<Action> newActions = new ArrayList<>();
+        for (Action action : runningActions) {
+            action.preview(packet.fieldOverlay());
+            if (action.run(packet)) {
+                newActions.add(action);
+            }
+        }
+        runningActions = newActions;
+
+        dash.sendTelemetryPacket(packet);
+
+
+
+
+        if (opmode.gamepad1.a) {
+            runningActions.add(new SequentialAction(
+                    new SleepAction(0.5)//,
+                    //new InstantAction(() -> servo.setPosition(0.5))
+            ));
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp.java
@@ -4,6 +4,7 @@ import static org.firstinspires.ftc.teamcode.Sensors.Obelisk.visionPortal;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.Aiming.Alignment;
 import org.firstinspires.ftc.teamcode.Mechanisms.DriveTrain;
 import org.firstinspires.ftc.teamcode.Sensors.Obelisk;
 
@@ -19,6 +20,7 @@ public class TeleOp extends LinearOpMode {
         //Init Functions
         DriveTrain.initDrive(this);
         Obelisk.initDetection(this);
+        Alignment.initAlignment(this);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -34,6 +36,7 @@ public class TeleOp extends LinearOpMode {
             //Loop Functions
             DriveTrain.updateDrive(gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_stick_x, gamepad1.right_trigger > 0.1);
             Obelisk.update();
+            Alignment.updateAlignment();
 
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.update();
