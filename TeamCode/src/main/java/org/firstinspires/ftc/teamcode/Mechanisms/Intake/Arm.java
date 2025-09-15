c673be0361b661f0d94c4dee8d2d75bd990557e7
BiggerBozo <cflibftc@gmail.com>
2025-09-15 14:43:02 -0700

    added intake roller, working on arm

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Intake/Arm.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.Mechanisms.Intake;
+
+public class Arm {
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Aiming.Alignment;
 import org.firstinspires.ftc.teamcode.Mechanisms.DriveTrain;
+import org.firstinspires.ftc.teamcode.Mechanisms.Intake.Roller;
 import org.firstinspires.ftc.teamcode.Sensors.Obelisk;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="9880 Decode TeleOp")
@@ -21,6 +22,7 @@ public class TeleOp extends LinearOpMode {
         DriveTrain.initDrive(this);
         Obelisk.initDetection(this);
         Alignment.initAlignment(this);
+        Roller.initIntake(this);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -37,6 +39,7 @@ public class TeleOp extends LinearOpMode {
             DriveTrain.updateDrive(gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_stick_x, gamepad1.right_trigger > 0.1);
             Obelisk.update();
             Alignment.updateAlignment();
+            Roller.updateIntake(gamepad2.right_trigger > 0.1, gamepad2.left_trigger > 0.1, 1.0);
 
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.update();
