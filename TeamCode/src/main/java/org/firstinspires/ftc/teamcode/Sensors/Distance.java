66342b3c592f8789587d5eb170e6bb1a006955a3
BiggerBozo <cflibftc@gmail.com>
2025-09-17 15:19:57 -0700

    distance sensor start

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensors/Distance.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.Sensors;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+public class Distance { // Prefix for commands
+    private static OpMode opmode; // opmode var init
+    private static Rev2mDistanceSensor sensorDistance;
+
+    public static void initSensor(OpMode opmode) {
+        sensorDistance = hardwareMap.get(Rev2mDistanceSensor.class, "distanceSensor");
+    }
+
+    public static void updateSensor() {
+        opmode.telemetry.addData("Distance Sensor", sensorDistance.getDistance(DistanceUnit.MM));
+    }
+}
