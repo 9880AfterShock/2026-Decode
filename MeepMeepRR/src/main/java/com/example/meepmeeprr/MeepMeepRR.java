94b1583919ceba75dc6e20dd7d4bc558c6ab83f3
BiggerBozo <cflibftc@gmail.com>
2025-10-07 15:52:17 -0700

    uhhhh

--- MeepMeepRR/src/main/java/com/example/meepmeeprr/MeepMeepRR.java
+++ MeepMeepRR/src/main/java/com/example/meepmeeprr/MeepMeepRR.java
@@ -1,12 +1,17 @@
 package com.example.meepmeeprr;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepRR {
     public static void main(String[] args) {
+        Pose2d forwardPos = new Pose2d(-30.0, 0.0, 0.0);
+        Pose2d backwardPos = new Pose2d(30.0, 0.0, 0.0);
+        Pose2d turnPos = new Pose2d(1.0, 1.0, -180.0);
+
         MeepMeep meepMeep = new MeepMeep(600);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
@@ -15,15 +20,10 @@ public class MeepMeepRR {
                 .build();
 
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
-                .lineToX(30)
-                .turn(Math.toRadians(90))
-                .lineToY(30)
-                .turn(Math.toRadians(90))
-                .lineToX(0)
-                .turn(Math.toRadians(90))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
-                .build());
+                .setTangent(Math.toRadians(0.0))
+                .splineToLinearHeading(forwardPos, Math.toRadians(0.0))
+                .build()
+        );
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_DARK)
                 .setDarkMode(true)
