cca2d6b0b21e5fb57eb1374fa47172e108c7afd6
Caden Ream <cadenream@cadencoaster.com>
2025-10-14 12:19:18 -0700

    more traj stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Maths/Trajectory.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Maths/Trajectory.java
@@ -2,14 +2,15 @@ package org.firstinspires.ftc.teamcode.Maths;
 
 public class Trajectory {
     public static LaunchInformation getVelocity(double x, double y, double wheel_radius, double angle) {
-        double gravity = 9.81;
+        double gravity = -9.81;
         double vel = Math.sqrt(
-                gravity*Math.pow(x,2)
+                (gravity*Math.pow(x,2))
                 / //-----------------
-                (Math.pow(Math.cos(angle),2)*(Math.tan(angle)-y))
-
+                ((Math.tan(angle)-y) * Math.pow(Math.cos(angle),2))
         );
-        double rpm = ((60)/(2*Math.PI*wheel_radius))*vel;
+        double rpm = ((60)
+                /
+                ((2*Math.PI)*wheel_radius))*vel;
         return new LaunchInformation(angle,vel,rpm);
     }
 }
