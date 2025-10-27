26b798f9f480de634b237063346c49b2820bee5d
rowan_mcalpin <rowan.mcalpin@gmail.com>
2025-10-27 14:59:24 -0700

    changed hood pos, had messed up and done 0.95 but fixed now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Scoring/Hood.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Scoring/Hood.java
@@ -8,7 +8,7 @@ import org.firstinspires.ftc.teamcode.Aiming.DriverTest;
 public class Hood {
     private static Servo hood; // init motor var
     private static OpMode opmode; // opmode var init
-    public static double farPosition = 0.97;
+    public static double farPosition = 0.98; //could be as "high" (low) as 0.97
     public static double nearPosition = 1.0;
     public static String hoodState = "Near";
 
