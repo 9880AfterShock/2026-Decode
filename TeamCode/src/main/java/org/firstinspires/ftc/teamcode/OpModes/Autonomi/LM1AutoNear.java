fa84050e736b69ee2c0b4f6ec12d9b0f7f5b7fae
rowan_mcalpin <rowan.mcalpin@gmail.com>
2025-11-05 12:07:31 -0800

    wait on near zone (scuffed_)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomi/LM1AutoNear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomi/LM1AutoNear.java
@@ -1,11 +1,7 @@
 
 package org.firstinspires.ftc.teamcode.OpModes.Autonomi;
 
-import androidx.annotation.NonNull;
-
 import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
@@ -13,7 +9,6 @@ import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.ftc.*;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Aiming.DriverTest;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
@@ -26,9 +21,8 @@ import org.firstinspires.ftc.teamcode.Systems.ActionManager;
 import org.firstinspires.ftc.teamcode.Systems.RunLater;
 
 @Config
-@Autonomous(name = "Far zone 3 not 6")
-public class LM1AutoFar extends LinearOpMode {
-    ElapsedTime runtime = new ElapsedTime();
+@Autonomous(name = "Near zone 3 not 6")
+public class LM1AutoNear extends LinearOpMode {
     @Override
     public void runOpMode() {
         //Mechs init'
@@ -41,10 +35,8 @@ public class LM1AutoFar extends LinearOpMode {
         ActionManager actionManager = new ActionManager( this, 28);
 
         QuickSpindexer.initSpindexer(this); //ugly but works
-
-        double waitTime = 0.0;
         double posMultiplier = 1.0;
-
+        double waitTime = 0.0;
         while (!isStopRequested() && !opModeIsActive()) {
             telemetry.addLine("Use dpad to change delay, and x and b to select alliance");
             if (gamepad1.xWasPressed()){
@@ -70,54 +62,45 @@ public class LM1AutoFar extends LinearOpMode {
                 telemetry.addData("Alliance", "Red");
             }
             telemetry.update();
-//            Obelisk.update();
         }
 
-        Pose2d startPosFar = new Pose2d(62.6, posMultiplier*-16.0, posMultiplier*Math.toRadians(0.0));
-        MecanumDrive drive = new MecanumDrive(hardwareMap, startPosFar);
+        Pose2d startPosClose = new Pose2d(-55.5, posMultiplier*-47.0, posMultiplier*Math.toRadians(55.0));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, startPosClose);
 
         //Poses
+        Pose2d scanPos = new Pose2d(-27.0, posMultiplier*-27.0, posMultiplier*Math.toRadians(-25.0));
+        Pose2d shootPosClose = new Pose2d(-30.0, posMultiplier*-33.0, posMultiplier*Math.toRadians(45));
+        Pose2d parkPosClose = new Pose2d(-60.0, posMultiplier*-35.0, posMultiplier*Math.toRadians(0.0));
         Pose2d startPickup1 = new Pose2d(-12.0, posMultiplier*-30.0, posMultiplier*Math.toRadians(-90.0));
         Pose2d endPickup1 = new Pose2d(-12.0, posMultiplier*-55.0, posMultiplier*-Math.toRadians(90.0));
-        Pose2d startPickup2 = new Pose2d(12.0, posMultiplier*-30.0, posMultiplier*-Math.toRadians(90.0));
-        Pose2d endPickup2 = new Pose2d(12.0, posMultiplier*-55.0, posMultiplier*-Math.toRadians(90.0));
         Pose2d startPickup3 = new Pose2d(35.5, posMultiplier*-30.0, posMultiplier*-Math.toRadians(90.0));
         Pose2d endPickup3 = new Pose2d(35.5, posMultiplier*-55.0, posMultiplier*-Math.toRadians(90.0));
         Pose2d gatePose = new Pose2d(0.0, posMultiplier*-55.0, posMultiplier*Math.toRadians(0.0));
-        Pose2d shootPosFar; //dif aiming angles
-        if (posMultiplier == 1){ //blue
-            shootPosFar = new Pose2d(57.5, posMultiplier*-12.0, posMultiplier*Math.toRadians(22.5));
-        } else { //red
-            shootPosFar = new Pose2d(57.5, posMultiplier * -12.0, posMultiplier * Math.toRadians(21.5));
-        }
         Pose2d parkPosFar = new Pose2d(37.75, posMultiplier*-32.75, posMultiplier*Math.toRadians(90.0));
 
-        TrajectoryActionBuilder waitOne = drive.actionBuilder(startPosFar)
-                .waitSeconds(1.0);
-        TrajectoryActionBuilder waitTwo = drive.actionBuilder(startPosFar)
-                .waitSeconds(2.0);
-        TrajectoryActionBuilder waitTwenty = drive.actionBuilder(startPosFar)
-                .waitSeconds(20.0);
-        TrajectoryActionBuilder toShoot1 = drive.actionBuilder(startPosFar)
-                .setTangent(posMultiplier*Math.toRadians(110.0))
-                .splineToLinearHeading(shootPosFar, posMultiplier*Math.toRadians(110.0));
-        TrajectoryActionBuilder toPickup1 = drive.actionBuilder(shootPosFar)
-                .setTangent(posMultiplier*Math.toRadians(-135.0))
-                .splineToLinearHeading(startPickup3, posMultiplier*Math.toRadians(-135));
-        TrajectoryActionBuilder pickup1 = drive.actionBuilder(startPickup3)
-                .setTangent(posMultiplier*Math.toRadians(-90.0))
-                .splineToLinearHeading(endPickup3,posMultiplier* Math.toRadians(-90.0));
-        TrajectoryActionBuilder toShoot2 = drive.actionBuilder(endPickup3)
+        //to scan
+        TrajectoryActionBuilder toScan = drive.actionBuilder(startPosClose)
+                .setTangent(posMultiplier*Math.toRadians(55.0))
+                .splineToLinearHeading(scanPos, posMultiplier*Math.toRadians(45.0));
+        TrajectoryActionBuilder toShoot1 = drive.actionBuilder(scanPos)
+                .setTangent(posMultiplier*Math.toRadians(-125.0))
+                .splineToLinearHeading(shootPosClose, posMultiplier*Math.toRadians(-125.0));
+        TrajectoryActionBuilder toPickup1 = drive.actionBuilder(shootPosClose)
                 .setTangent(posMultiplier*Math.toRadians(45.0))
-                .splineToLinearHeading(shootPosFar, posMultiplier*Math.toRadians(45.0));
-        TrajectoryActionBuilder toPark = drive.actionBuilder(shootPosFar)
-                .setTangent(posMultiplier*Math.toRadians(-135.0))
-                .splineToLinearHeading(parkPosFar, posMultiplier*Math.toRadians(-135.0));
-        TrajectoryActionBuilder waitVariable = drive.actionBuilder(startPosFar)
+                .splineToLinearHeading(startPickup1, posMultiplier*Math.toRadians(-45.0));
+        TrajectoryActionBuilder pickup1 = drive.actionBuilder(startPickup1)
+                .setTangent(posMultiplier*Math.toRadians(-90.0))
+                .splineToLinearHeading(endPickup1, posMultiplier*Math.toRadians(-90.0));
+        TrajectoryActionBuilder toShoot2 = drive.actionBuilder(startPickup1)
+                .setTangent(posMultiplier*Math.toRadians(125.0))
+                .splineToLinearHeading(shootPosClose, posMultiplier*Math.toRadians(125.0));
+        TrajectoryActionBuilder toPark = drive.actionBuilder(shootPosClose)
+                .setTangent(posMultiplier*Math.toRadians(180.0))
+                .splineToLinearHeading(parkPosClose, posMultiplier*Math.toRadians(-180.0));
+        TrajectoryActionBuilder waitVariable = drive.actionBuilder(startPosClose)
                 .waitSeconds(waitTime);
 
         waitForStart();
-        runtime.reset();
 
         if (isStopRequested()) return;
 
@@ -125,33 +108,32 @@ public class LM1AutoFar extends LinearOpMode {
                 new SequentialAction(
                         actionManager.shotCue(0),
                         Arm.AutoArmIn(),
-                        Hood.AutoHoodFar(),
+                        Hood.AutoHoodNear(),
+                        toScan.build(),
+                        Obelisk.AutoScan(),
                         new ParallelAction(
                                 new SequentialAction(
                                         actionManager.shotCue(1),
-                                        Obelisk.AutoScan(),
                                         actionManager.spindexer.goToMotif(),
                                         actionManager.cycleRamp()
                                 ),
                                 toShoot1.build()
                         ),
 
-                        waitVariable.build(), //actionmanager one doesnt work
-
-                        //new block
-                        actionManager.rev(4100),
+                        waitVariable.build(),
+                        actionManager.rev(3000),
 //                        waitOne.build(),
-                        actionManager.waitForSpeedSafe(4100),
+                        actionManager.waitForSpeedSafe(3000),
                         actionManager.launch(),
 
                         actionManager.shotCue(2),
                         QuickSpindexer.turnRight(),
-                        actionManager.waitForSpeedSafe(4100),
+                        actionManager.waitForSpeedSafe(3000),
                         actionManager.launch(),
 
                         actionManager.shotCue(3),
                         QuickSpindexer.turnRight(),
-                        actionManager.waitForSpeedSafe(4100),
+                        actionManager.waitForSpeedSafe(3000),
                         actionManager.launch(),
 
                         actionManager.derev(),
