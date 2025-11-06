375a07078a384d3184b1573d652f15e75f89e1e4
rowan_mcalpin <rowan.mcalpin@gmail.com>
2025-11-06 13:46:52 -0800

    spindexer thingy auto sqworint stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Sorting/QuickSpindexer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Sorting/QuickSpindexer.java
@@ -120,13 +120,12 @@ public class QuickSpindexer { // Prefix for commands
             public boolean run(@NonNull TelemetryPacket telemetryPacket) {
                 if (first) {
                     if (Obelisk.motif != currentInventory) {
-                        if (Obelisk.motif == Motif.GPP){
-                            if (currentInventory == Motif.PGP){
-//ugh this is gonna be a pain, not done yet
-                            }
+                        if ((Obelisk.motif == Motif.GPP && (currentInventory == Motif.PPG)) || (Obelisk.motif == Motif.PGP)&& (currentInventory == Motif.GPP) || (Obelisk.motif == Motif.PPG)&& (currentInventory == Motif.PGP)){
+                            spindexer.setTargetPosition((int) (spindexer.getTargetPosition()+(1425.1/3)));
                         }
-
-                        spindexer.setTargetPosition((int) (spindexer.getTargetPosition()+(1425.1/3)));
+                        if ((Obelisk.motif == Motif.GPP && (currentInventory == Motif.PGP)) || (Obelisk.motif == Motif.PGP)&& (currentInventory == Motif.PPG) || (Obelisk.motif == Motif.PPG)&& (currentInventory == Motif.GPP)){
+                            spindexer.setTargetPosition((int) (spindexer.getTargetPosition()-(1425.1/3)));
+                        }
                     }
                     first = false;
                 }
