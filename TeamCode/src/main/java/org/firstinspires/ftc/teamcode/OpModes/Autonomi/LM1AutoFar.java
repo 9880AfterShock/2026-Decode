5592662555a4b3eb14cb52c044f369b84d11b580
rowan_mcalpin <rowan.mcalpin@gmail.com>
2025-11-12 11:09:29 -0800

    whooooo #4 in interleagueadb disconnectadb disconnect

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomi/LM1AutoFar.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomi/LM1AutoFar.java
@@ -1,4 +1,3 @@
-
 package org.firstinspires.ftc.teamcode.OpModes.Autonomi;
 
 import androidx.annotation.NonNull;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomi/LM1AutoNearSmall.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomi/LM1AutoNearSmall.java
@@ -1,4 +1,3 @@
-
 package org.firstinspires.ftc.teamcode.OpModes.Autonomi;
 
 import com.acmerobotics.dashboard.config.Config;
