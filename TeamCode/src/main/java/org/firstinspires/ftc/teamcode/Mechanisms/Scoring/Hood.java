02ef11f5feafd908d42e183364dc444351e2c372
rowan_mcalpin <rowan.mcalpin@gmail.com>
2025-12-15 14:37:02 -0800

    timo changes, need to have more tuned curve fit sitll

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Scoring/Hood.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Scoring/Hood.java
@@ -13,7 +13,7 @@ public class Hood {
     private static Servo hood; // init motor var
     private static OpMode opmode; // opmode var init
     public static double farPosition = 0.96; //could be as "high" (low) as 0.97 //old 0.96
-    public static double nearPosition = 0.98; //old 0.98
+    public static double nearPosition = 0.98; //old 0.98 ---- Note to Timo: 0.96 is far, 0.98 is near
     public static String hoodState = "Near";
 
     public static void initAim(OpMode opmode) { // init motor
