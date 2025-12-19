126e939e0def6676151c5466bfa9eb0cbbdedada
rowan_mcalpin <rowan.mcalpin@gmail.com>
2025-12-19 12:53:03 -0800

    added kickstand

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Hinge.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.Mechanisms;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Hinge {
+    private static Servo hingeLeft; // init servo var
+    private static Servo hingeRight; // init servo var
+    private static OpMode opmode; // opmode var init
+    public static double inPosition = 0.0;
+    public static double outPosition = 1.0;
+    public static boolean lifting = false;
+
+    public static void initAim(OpMode opmode) { // init motor
+        hingeLeft = opmode.hardwareMap.get(Servo.class, "hingeLeft"); //Port _ on _ hub
+        hingeRight = opmode.hardwareMap.get(Servo.class, "hingeRight"); //Port _ on _ hub
+        Hinge.opmode = opmode;
+        lifting = false;
+    }
+
+    public static void updateBase(boolean toggleBase) {
+        if (toggleBase){
+            if (lifting) {
+                lifting = false;
+            } else {
+                lifting = true;
+            }
+        }
+
+        if (lifting) {
+            hingeLeft.setPosition(outPosition);
+            hingeRight.setPosition(outPosition);
+        } else {
+            hingeLeft.setPosition(inPosition);
+            hingeRight.setPosition(inPosition);
+        }
+
+        opmode.telemetry.addData("Returning to Base?", lifting);
+    }
+}
