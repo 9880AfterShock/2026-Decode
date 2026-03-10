40e0efa11f73521206dac9cef2d7a39bcf0ba433
BiggerBozo <cflibftc@gmail.com>
2026-03-10 15:33:33 -0700

    fixing control of offset

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/ControlManager.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/ControlManager.java
@@ -121,7 +121,7 @@ public class ControlManager {
             cyclePrepped = false;
         }
 
-        if (operator.aWasPressed() && !(operator.left_trigger > 0.5)){
+        if (operator.aWasPressed() /*&& !(operator.left_trigger > 0.5)*/){
             QuickSpindexer.spindexerOffset = true;
         } else if (operator.aWasReleased()) {
             QuickSpindexer.spindexerOffset = false;
