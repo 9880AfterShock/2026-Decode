90c95ea3edd6596ba7ea0694f55a471ae28a5ff2
BiggerBozo <cflibftc@gmail.com>
2026-03-13 11:18:22 -0700

    die magic numbers

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/DriveTrain.java
@@ -45,6 +45,10 @@ public class DriveTrain { // Prefix for commands
     private static PIDAbstract aimingPID;
     private static Pose2d pos;
     public static TwoDeadWheelLocalizer localizer;
+    public static double nearP = -0.02;
+    public static double nearD = 0.008;
+    public static double farP = -0.01;
+    public static double farD = 0.008;
     private static Pose2d goalTarget = new Pose2d(-58.0, -57.0, Math.toRadians(0.0));
 
     public static void initDrive(OpMode opmode) { // init motors
@@ -85,8 +89,8 @@ public class DriveTrain { // Prefix for commands
 
         goalTarget = new Pose2d(-60.0, -53.0, Math.toRadians(0.0));
 
-        aimingPID = new MultiPID(new PID(-0.01,0.0,0.008,1)) //Normal PID
-                .addPID(new PID(-0.02,0.0,0.008,1),10); //Close Range PID
+        aimingPID = new MultiPID(new PID(farP,0.0, farD,1)) //Normal PID
+                .addPID(new PID(nearP,0.0,nearD,1),10); //Close Range PID
 
 
     }
