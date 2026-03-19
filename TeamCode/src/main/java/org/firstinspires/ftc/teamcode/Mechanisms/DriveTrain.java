5fac5cbf33e964c8b314e38a29be3205392b651f
BiggerBozo <cflibftc@gmail.com>
2026-03-19 12:01:21 -0700

    minor tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/DriveTrain.java
@@ -47,8 +47,8 @@ public class DriveTrain { // Prefix for commands
     private static PIDAbstract aimingPID;
     private static Pose2d pos;
     public static TwoDeadWheelLocalizer localizer;
-    public static double nearP = -0.02;
-    public static double nearD = 0.008;
+    public static double nearP = -0.025;
+    public static double nearD = 0.0085;
     public static double farP = -0.007;
     public static double farD = 0.008;
     private static Pose2d goalTarget = new Pose2d(-58.0, -57.0, Math.toRadians(0.0));
