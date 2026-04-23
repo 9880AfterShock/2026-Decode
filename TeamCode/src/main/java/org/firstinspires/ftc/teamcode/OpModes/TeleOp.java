e06d01e796c7bc01db090638eac512aa0d1b5010
BiggerBozo <cflibftc@gmail.com>
2026-04-23 15:41:47 -0700

    turret looptimes harder on the teleop than on the solo turret

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp.java
@@ -107,7 +107,19 @@ public class TeleOp extends LinearOpMode {
             RunLater.update();
             RunCondition.update();
 //            BallColorDetectinator.update();
+
             Turret.updateTurret(false, 0.0);
+//            /*
+            if (gamepad2.left_stick_y == 0 && gamepad2.left_stick_x == 0){
+                Turret.updateTurret(gamepad2.left_stick_button, 0.0);
+            } else {
+                if (gamepad1.left_stick_y < 0){
+                    Turret.updateTurret(gamepad2.left_stick_button, 90+Math.toDegrees(Math.atan2(gamepad2.left_stick_y, gamepad2.left_stick_x)));
+                } else{
+                    Turret.updateTurret(gamepad2.left_stick_button, 90+Math.toDegrees(Math.atan2(-gamepad2.left_stick_y, -gamepad2.left_stick_x)));
+                }
+            }//*/
+
             Distance.updateSensor();
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Alliance", alliance);
