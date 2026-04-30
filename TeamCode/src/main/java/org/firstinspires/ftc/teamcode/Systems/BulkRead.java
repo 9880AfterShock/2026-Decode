3eb59cfb5c6cd87a52c41c0a5aa22c8169396882
BiggerBozo <cflibftc@gmail.com>
2026-04-30 12:58:40 -0700

    framework setup

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/BulkRead.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.Systems;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class BulkRead {
+
+
+    public static void initBulkRead(OpMode opmode) {
+
+    }
+
+    public static void bulkRead(){ //cache data
+
+    }
+
+    public static void limeLightUpdate(){ //for after bulk read, but before anything else, write to LL and fetch fidicuial results
+
+    }
+
+    public static void bulkWrite(){ //all writing new motor speed and stuff
+
+    }
+}
