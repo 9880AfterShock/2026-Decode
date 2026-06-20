3394b17defccc3c63b5775dd658ab612000b1a3b
BiggerBozo <cflibftc@gmail.com>
2026-06-20 06:33:57 -0700

    sensorange test commented out shooter motor test (DONT GRIND GEARS)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/SensOrangeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/SensOrangeTest.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Mechanisms.Scoring.Turret;
@@ -15,6 +16,8 @@ public class SensOrangeTest extends LinearOpMode {
 
         //Init Functions
         SensOrange.initSensor(this);
+//        DcMotorEx shooterUp = hardwareMap.get(DcMotorEx.class, "shooterUp"); //Port 3 or 4 on the expansion hub //one with encoder
+//        DcMotorEx shooterDown = hardwareMap.get(DcMotorEx.class, "shooterDown"); //Port 3 or 4 on the expansion hub //one without encoder
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -25,6 +28,8 @@ public class SensOrangeTest extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
+//            shooterUp.setPower(gamepad1.left_stick_x);
+//            shooterDown.setPower(gamepad1.right_stick_x);
             SensOrange.updateEncoder();
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.update();
